4f48d3a8e55f20efd810f9eafb1c9cb0df38fd51
sumitpandit <sumitpandit178@gmail.com>
2020-09-28 15:18:42 +0530

    [Sumit] Add.Employee Wage Calculation

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -1,16 +1,43 @@
 ﻿
-//UC-1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//UC-2
+
 
 using System;
-
-namespace EmployeeWage
+namespace EmployeeWagesUC2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program");
-
             int Full_Time = 2;
             int Emp_rate = 20;
             int Emp_Hour, Emp_Wages;
@@ -18,10 +45,12 @@ namespace EmployeeWage
             int check = r.Next(1, 3);
             if (check == Full_Time)
             {
-                Console.WriteLine("Employee is present");
+                Emp_Hour = 8;
             }
             else
-                Console.WriteLine("Employee Wages is absent");
+                Emp_Hour = 0;
+            Emp_Wages = Emp_Hour * Emp_rate;
+            Console.WriteLine("Employee Wages: " + Emp_Wages);
         }
     }
 }
