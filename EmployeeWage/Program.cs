9a49908aeaf09df24b43610a15c5ee0ef03237e8
sumitpandit <sumitpandit178@gmail.com>
2020-09-28 15:08:49 +0530

    [Sumit] Add. UC-1 Employee Present or not

---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+
+            int Full_Time = 2;
+            int Emp_rate = 20;
+            int Emp_Hour, Emp_Wages;
+            Random r = new Random();
+            int check = r.Next(1, 3);
+            if (check == Full_Time)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+                Console.WriteLine("Employee Wages is absent");
+        }
+    }
+}
